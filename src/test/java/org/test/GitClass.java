8defd2c169d74d1738bde1449fa3bcc67028ca28
sumithrajeeva <sumithrarms94@gmail.com>
2023-01-24 12:25:35 +0530

    Hi

---
+++ src/test/java/org/test/GitClass.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class GitClass {
+	private void Chennai() {
+		System.out.println("Chennai");
+	}
+	private void Madurai() {
+		System.out.println("Madurai");
+	}
+	private void Salem() {
+		System.out.println("Salem");
+	}
+
+}
